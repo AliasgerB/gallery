12c105449ca9477017a5355966125524ce10874d
ali <bagaliasger53@gmail.com>
2022-06-30 21:24:20 +0530

    master

---
+++ src/Callmenu.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+export const Callmenu = ({ filterData, callItem }) => {
+  return (
+    <>
+      <h5>Company</h5>
+      <article className="companies">
+        {callItem.map((elem, index) => {
+          return (
+            <button
+              key={index}
+              className="company-btn"
+              onClick={() => {
+                filterData(`${elem}`);
+              }}
+            >
+              {elem}
+            </button>
+          );
+        })}
+      </article>
+
+      {/* <button
+          className="company-btn"
+          onClick={() => {
+            setitems(Products);
+          }}
+        >
+          all
+        </button> */}
+      {/* <button
+        className="company-btn"
+        onClick={() => {
+          filterData("ikea");
+        }}
+      >
+        ikea
+      </button>
+      <button
+        className="company-btn"
+        onClick={() => {
+          filterData("marcos");
+        }}
+      >
+        marcos
+      </button> */}
+    </>
+  );
+};
